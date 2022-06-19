365b780ef5f76d6aaca3b624e1e96b7eaff3ad54
chaandrey <88089302+chaandrey@users.noreply.github.com>
2022-06-20 02:01:30 +0300

    Add files via upload

---
+++ db_service.py
@@ -0,0 +1,83 @@
+import sqlite3
+import logging
+
+logging.basicConfig( 
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', 
+    filename='logs.log', 
+    level=logging.INFO) 
+
+def connection_to_db():
+    connec=sqlite3.connect('users1.db')
+    return connec
+
+def main():
+    try:
+        connec = connection_to_db()
+        connec.execute('''CREATE TABLE IF NOT EXISTS users3(
+            chat_id INT,
+            name CHAR,
+            collection CHAR,
+            current_price INTEGER,
+            percentage INTEGER,
+            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP);''')
+        logging.info('Connected to table successfully!') 
+    except:
+        print('Failed to connect to table!')
+
+
+def add_user_to_db(chat_id, name, collection, current_price, percentage):
+    """Adding user to DB."""
+    try:
+        connec = connection_to_db()
+        connec.execute("INSERT INTO users3(chat_id, name, collection, current_price, percentage) VALUES(?, ?, ?, ?, ?)", (chat_id, name, collection, current_price, percentage));
+        connec.commit()
+        logging.info('Record was added successfully: {}, {}, {}, {}, {}'.format(chat_id, name, collection, current_price, percentage)) 
+    except sqlite3.IntegrityError as err:
+        logging.error('User {}, {} was not added {}'.format(chat_id, name, err)) 
+
+
+def get_collection(chat_id):
+    """Get collection for specific user."""
+    connec = connection_to_db()
+    output = connec.execute("SELECT collection FROM users3 WHERE chat_id = {}".format(chat_id))
+    data_rows = output.fetchall()
+    final_data = [i[0] for i in data_rows]
+    return final_data
+
+
+def check_user(chat_id):
+    """Check if user exists in DB."""
+    connec = connection_to_db()
+    output = connec.execute("SELECT * FROM users3 WHERE chat_id = {}".format(chat_id))
+    if output.fetchall():
+        return True
+
+
+def get_all_collections(chat_id):
+    """Get all collection for particular user."""
+    connec = connection_to_db()
+    output = connec.execute("SELECT collection FROM users3 WHERE chat_id = {}".format(chat_id))
+    return output.fetchall()
+
+
+def check_unique(chat_id, new):
+    """Check if new collection was not added previously."""
+    connec = connection_to_db()
+    output = connec.execute("SELECT * FROM users3 WHERE collection = {}".format(chat_id, new))
+    if output.fetchall():
+        return True
+
+
+def get_users_for_update(): # need to add group by in order to update all users with same collection
+    """Fetch all users that require update."""
+    connec = connection_to_db()
+    output = connec.execute("SELECT * FROM users3 WHERE percentage != 0 ORDER BY timestamp ASC")
+    output = output.fetchone()
+    return output
+
+
+def update_record(chat_id, collection, updated_price):
+    """Updating record with new price."""
+    connec = connection_to_db()   # need to update timestamp as well.
+    connec.execute("UPDATE users3 SET current_price = {} WHERE chat_id = {} AND collection = {}".format(updated_price, chat_id, collection))
+    connec.commit()
---
+++ main.py
@@ -0,0 +1,102 @@
+import logging
+import db_service
+import api_os
+from telegram import *
+from telegram.ext import *
+
+
+TELEGRAM_TOKEN = '5399837848:AAFPhYs_DGb44UmxgE_gZC3xKPJXgGdKY-g'
+TELEGRAM_CHAT_ID = 396296669
+
+updater = Updater(token=TELEGRAM_TOKEN)
+bot = Bot(token=TELEGRAM_TOKEN)
+
+db_service.main()
+
+def wake_up(update, context):
+    """Starting Bot."""
+    chat = update.effective_chat
+    name = update.message.chat.first_name
+    if db_service.check_user(chat.id):
+        buttons = ReplyKeyboardMarkup([['/AddCollection',
+        '/GetPrice',
+        '/SetNotifications',
+        '/GetCollections'
+        ]], resize_keyboard=True)
+    else:
+        buttons = ReplyKeyboardMarkup([['/AddCollection']], resize_keyboard=True)
+    context.bot.send_message(
+        chat_id=chat.id,
+        text='{}, choose an option!'.format(name),
+        reply_markup=buttons  
+        )
+
+
+def add_collection(update, context):
+    updater.dispatcher.add_handler(MessageHandler(Filters.text, addingNFT))
+    chat = update.effective_chat
+    send_message(chat.id, 'Please enter name of NFT collection')
+
+
+def send_message(chat_id, message):
+    bot.send_message(chat_id, message)
+
+
+def addingNFT(update, context):
+    chat = update.effective_chat
+    name = update.message.chat.first_name
+    message_text = update['message']['text']
+    try:
+        collection, percentage = message_text.split()
+        collection = collection.lower()
+    except:
+        collection = message_text
+    if api_os.check_collection(collection):
+        if db_service.check_unique(chat.id, collection):
+            logging.info('{} was already added by {}'.format(collection, name))
+            send_message(chat.id, '{} was already added.'.format(collection))
+        else:
+            if 'percentage' in locals():
+                price = api_os.get_price(collection)
+                db_service.add_user_to_db(chat.id, name, collection, price, percentage)
+                send_message(chat.id,'{} was added. Will update on every {}% change'.format(collection, percentage))
+            else:
+                price = api_os.get_price(collection)
+                db_service.add_user_to_db(chat.id, name, collection, price, 0)
+                send_message(chat.id, '{} was added. Notifications are disabled'.format(collection))
+            wake_up(update, context)
+    else:
+        send_message(chat.id, '{} was not found.'.format(collection))
+        wake_up(update, context)
+
+
+def get_price(update, context):
+    chat = update.effective_chat
+    collections = db_service.get_collection(chat.id)
+    for collection in collections:
+        price = api_os.get_price(collection)
+        message = 'Current FP for {} is {} ETH'.format(collection, price)
+        send_message(chat.id, message)
+        logging.info('FP {} ETH, {}, was sent to {} '.format(price, collection, chat.first_name))
+
+
+def set_notifications(chat_id):
+    pass
+
+def get_collections(update, context):
+    chat = update.effective_chat
+    output = db_service.get_all_collections(chat.id)
+    send_message(chat.id, output)
+
+
+
+updater.dispatcher.add_handler(CommandHandler('start', wake_up))
+updater.dispatcher.add_handler(CommandHandler('AddCollection', add_collection))
+updater.dispatcher.add_handler(CommandHandler('GetPrice', get_price))
+updater.dispatcher.add_handler(CommandHandler('SetNotifications', set_notifications))
+updater.dispatcher.add_handler(CommandHandler('GetCollections', get_collections))
+
+
+updater.start_polling()
+updater.idle() 
+
